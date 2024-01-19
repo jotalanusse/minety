eff6ff90b78f7c0c47897893f3cdc84cfecec322
Jota Lanusse <jotalanusse@gmail.com>
2024-01-19 13:07:19 -0300

    feat: deleted old files

--- archived/common/create_chunk.py
+++
@@ -1,9 +0,0 @@
-# Create the chunk from a given chunk object
-def create_chunk(chunk_object):
-  chunk = {
-    'inhabited_ticks': chunk_object.data['InhabitedTime'].value,
-    'x_pos': chunk_object.data['xPos'].value,
-    'z_pos': chunk_object.data['zPos'].value
-  }
-  
-  return chunk
